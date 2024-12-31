43a4bf1fc25cbab8005e3e696b9c91a405fab45c
royappa <royappa@spiralfx.com>
2024-12-30 21:09:45 -0500

    Day 23 B

--- AdventOfCode2024/p23b.cpp
+++ AdventOfCode2024/p23b.cpp
@@ -22,33 +22,55 @@ vector<string> split_nonre(string s, string sep)
 	return results;
 }
 
+int maxClique;
+string best;
+
+void BronKerbosch(set<string> R, set<string> P, set<string> X, map<string,set<string>> &graph) {
+  if (P.size() == 0 && X.size() == 0) {
+    if (R.size() > maxClique) {
+      maxClique = R.size();
+      best = "";
+      int num = 0;
+      for (auto x : R) {
+        best += x;
+        num++;
+        if (num != R.size()) 
+          best += ",";
+      }
+    }
+    return;
+  }
+  
+  while (P.size() > 0) {
+    string v = *(P.begin());
+    set<string> NR = R;
+    NR.insert(v);
+    set<string> PV, XV;
+    set_intersection(P.begin(), P.end(), graph[v].begin(), graph[v].end(), inserter(PV, PV.begin()));
+    set_intersection(X.begin(), X.end(), graph[v].begin(), graph[v].end(), inserter(XV, XV.begin()));
+    BronKerbosch(NR, PV, XV, graph);
+    P.erase(v);
+    X.insert(v);
+  }
+}
+
 int main(int argc, char *argv[])
 {
   if (argc > 1) debug = atoi(argv[1]);
  
   string line;
   map<string,set<string>> graph;
-  set<string> ts;
+  set<string> nodes;
   while (getline(cin, line)) {
     vector<string> w = split_nonre(line, "-");
     sort(w.begin(), w.end());
-    copy_if(w.begin(), w.end(), inserter(ts, ts.end()), 
-            [](string s) {return s[0] == 't';}
-    );
+    nodes.insert(w.begin(), w.end());
     graph[w[0]].insert(w[1]);
     graph[w[1]].insert(w[0]);
   }
-  int num = 0;
-  set<set<string>> threes;
-  for (auto t : ts) {
-    for (auto x : graph[t]) {
-      for (auto y : graph[t]) {
-        if (graph[x].count(y)) {
-          threes.insert({t,x,y});
-        }
-      }
-    }
-  }
-  cout << threes.size() << endl;
+  
+  maxClique = 0;
+  BronKerbosch({}, nodes, {}, graph);
+  cout << maxClique << " " << best << endl;
 }
 
