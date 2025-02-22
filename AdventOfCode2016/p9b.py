ecc63b2282e7963d392876dd3d58bb67b83043ce
royappa <royappa@spiralfx.com>
2025-02-22 17:08:03 -0500

    2016 Day 9

---
+++ AdventOfCode2016/p9b.py
@@ -0,0 +1,32 @@
+import sys
+import re
+from collections import defaultdict
+from collections import Counter
+import hashlib
+
+def decomp(s):
+  if len(s) == 0:
+    return 0
+  tot = 0
+  while len(s) > 0:
+    match = re.search(r"(.*?)\((\d+)x(\d+)\)(.*)", s)
+    if match:
+      tot += len(match.group(1))
+      a = int(match.group(2))
+      b = int(match.group(3))
+      rem = match.group(4)
+      tot += b*decomp(rem[:a])
+      s = rem[a:]
+    else:
+      tot += len(s)
+      s = ""
+  return tot
+
+lines = [line for line in sys.stdin]
+s = "".join(line.strip() for line in lines)
+
+tot = decomp(s)
+
+print(tot)
+ 
+    
